77a02fc0027743c01819053df297cbeff1774c0b
kach9201 <116523970+kach9201@users.noreply.github.com>
2023-04-02 21:51:23 -0400

    tried to fix recipes

--- controllers/apiRoutes/recipeRoutes.js
+++ controllers/apiRoutes/recipeRoutes.js
@@ -2,11 +2,6 @@ const express = require('express');
 const router = express.Router();
 const recipes = require('../apiRoutes'); // Load the data from the seeds file
 
-// Define routes here
-router.get('/', (req, res) => {
-  res.render('recipe', { recipes }); // Pass the data to the home handlebars template
-});
-
 
 const { Connection } = require('pg');
 // const router = require('express').Router();
@@ -16,12 +11,14 @@ const withAuth = require('../../utils/helpers');
 
 
 
-// // Get all recipes
+// Get one recipe by name
 
 router.get('/', async (req, res) => {
   try {
-    const recipe = await Recipe.findAll();
-    res.render('recipe',);// { recipes });
+    const recipe = await Recipe.findOne({ where: { title: req.body.title } });
+    // res.render('recipe',);// { recipes });
+    console.log("HELLO FROM THE RECIPE GET")
+    res.status(200).json(recipe)
   } catch (error) {
     console.error(error);
     res.status(500).send('Server error home ln 16');
@@ -59,7 +56,6 @@ router.post('/', async (req, res) => {
     res.status(500).send('Server error ln 47');
   }
 });
-
 
 // // Update a recipe
 router.post('/create', async (req, res) => {
@@ -118,142 +114,3 @@ router.delete('/:id', async (req, res) => {
 });
 
 module.exports = router;
-
-// router.get('/', async (req, res) => {
-//   try {
-//     const recipes = await new Recipe.findAll();
-//     res.render('/recipe',);// { recipes });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Server error home ln 16');
-//   }
-// });
-
-
-
-
-// router.get('/', async (req, res) => {
-//   try {
-//     // Get all projects and JOIN with user data
-//     const recipeData = await Recipe.findAll({
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//     });
-
-//     // Serialize data so the template can read it
-//     const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
-
-//     // Pass serialized data and session flag into template
-//     res.render('recipe', {
-//       recipes,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-
-
-
-
-
-// router.get('/recipe', (req, res) => {
-//   router.get('/', async (req, res) => {
-//     try {
-//         const recipes = await recipe.findAll();
-//        res.render('home', { recipes });
-//       } catch (error) {
-//         console.error(error);
-//         res.status(500).send('Server error');
-//        }
-//      });
-// });
-
-// router.post('/recipe', (req, res) => {
-//   // Handle POST request for recipes
-// });
-
-// module.exports = router;
-// const express = require('express');
-// const router = express.Router();
-// const recipe = require('../../models/recipe');
-
-
-
-// // Get a single recipe by ID
-// router.get('/:id', async (req, res) => {
-//   try {
-//     const recipe = await Recipe.findByPk(req.params.id);
-//     if (recipe) {
-//       res.render('recipe', { recipe });
-//     } else {
-//       res.status(404).send('Recipe not found');
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Server error');
-//   }
-// });
-
-// // Create a new recipe
-// router.post('/', async (req, res) => {
-//   try {
-//     const { name, description, ingredients, instructions, image } = req.body;
-//     const recipe = await Recipe.create({
-//       name,
-//       description,
-//       ingredients,
-//       instructions,
-//       image
-//     });
-//     res.redirect(`/recipes/${recipe.id}`);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Server error');
-//   }
-// });
-
-// // Update a recipe by ID
-// router.put('/:id', async (req, res) => {
-//   try {
-//     const { name, description, ingredients, instructions, image } = req.body;
-//     const recipe = await Recipe.findByPk(req.params.id);
-//     if (recipe) {
-//       recipe.name = name;
-//       recipe.description = description;
-//       recipe.ingredients = ingredients;
-//       recipe.instructions = instructions;
-//       recipe.image = image;
-//       await recipe.save();
-//       res.redirect(`/recipes/${recipe.id}`);
-//     } else {
-//       res.status(404).send('Recipe not found');
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Server error');
-//   }
-// });
-
-// // Delete a recipe by ID
-// router.delete('/:id', async (req, res) => {
-//   try {
-//     const recipe = await Recipe.findByPk(req.params.id);
-//     if (recipe) {
-//       await recipe.destroy();
-//       res.redirect('/recipes');
-//     } else {
-//       res.status(404).send('Recipe not found');
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Server error');
-//   }
-// });
-
-// module.exports = recipeRoutes;
--- public/js/recipe.js
+++ public/js/recipe.js
@@ -1,52 +1,23 @@
-class Recipe {
-    constructor(title, description, ingredients, directions) {
-        this.title = title;
-        this.description = description;
-        this.ingredients = ingredients;
-        this.directions = directions;
+const mealFormHandler = async(event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#search-input').value.trim();
+
+    const searchRecipe = {
+        title : name
     }
 
-    printRecipe() {
-        console.log(`Recipe: ${this.title}`);
-        console.log(`Description: ${this.description}`);
-        console.log(`Ingredients: ${this.ingredients}`);
-        console.log(`directions: ${this.directions}`);
+        const response = await fetch('/api/recipe', {
+            body: JSON.stringify(searchRecipe),
+            method: 'GET',
+            headers: {
+            'content-type': 'application/json',
+            },
+        });
+        const data = await response.json();
+        console.log(data);
+
     }
-};
-
-
-const recipeContainer = document.querySelector('.recipe-list');
-const searchForm = document.querySelector('#search-form input[type="submit"]');
-
-searchForm.addEventListener('click', (e) => {
-
-    e.preventDefault();
-    const searchTerm = document.querySelector('#search-input').value
-    console.log('Search form clicked', "searchTerm", searchTerm);
-    try {
-        const recipeData = await Recipe.findbypk
-    }
-});
-
-
-async function getRecipes() {
-    const response = await fetch("/api/recipe");
-    const recipes = await response.json();
-    console.log(recipes);
-    response.forEach(recipe => {
-        const newRecipe = new Recipe(recipe.title, recipe.description, recipe.ingredients, recipe.directions);
-        recipeContainer.innerHTML += newRecipe.toHTML();
-    });
-}
-getRecipes();
-
-// recipeContainer.innerHTML = recipes;
-
-recipeContainer.innerHTML = printRecipe.toHTML();
-
-printRecipe = (recipe) => {
-    recipe.printRecipe();
-};
 
 
 
